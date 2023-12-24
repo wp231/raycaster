05fba02cd2a2370d0e916dcfdf58a2aae7f866e0
Samuel <samuelcat1478@gmail.com>
2023-12-24 16:04:04 +0800

    add doc

--- main_sdl.c
+++ main_sdl.c
@@ -8,6 +8,12 @@
 #include "raycaster_float.h"
 #include "renderer.h"
 
+// 函數：draw_buffer
+// 參數：sdlRenderer - SDL 渲染器
+//       sdlTexture - SDL 紋理
+//       fb - 彩色緩衝區
+//       dx - x方向偏移
+// 說明：將彩色緩衝區的內容渲染到 SDL 窗口上
 static void draw_buffer(SDL_Renderer *sdlRenderer,
                         SDL_Texture *sdlTexture,
                         uint32_t *fb,
@@ -29,6 +35,12 @@ static void draw_buffer(SDL_Renderer *sdlRenderer,
     SDL_RenderCopy(sdlRenderer, sdlTexture, NULL, &r);
 }
 
+// 函數：process_event
+// 參數：event - SDL 事件
+//       moveDirection - 移動方向
+//       rotateDirection - 旋轉方向
+// 返回：如果事件為退出事件，返回 true；否則返回 false
+// 說明：處理 SDL 事件，並更新移動和旋轉方向
 static bool process_event(const SDL_Event *event,
                           int *moveDirection,
                           int *rotateDirection)
@@ -61,11 +73,19 @@ static bool process_event(const SDL_Event *event,
     }
     return false;
 }
+
+// 主函數：main
+// 參數：argc - 命令行參數數量
+//       args - 命令行參數
+// 返回：程式退出碼
+// 說明：程式的主入口點
 int main(int argc, char *args[])
 {
+    // 初始化 SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
     } else {
+        // 創建 SDL 窗口
         SDL_Window *sdlWindow =
             SDL_CreateWindow("RayCaster [fixed-point vs. floating-point]",
                              SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
@@ -76,6 +96,7 @@ int main(int argc, char *args[])
             printf("Window could not be created! SDL_Error: %s\n",
                    SDL_GetError());
         } else {
+            // 初始化遊戲和光線追踪器
             Game game;
             RayCaster *floatCaster = RayCasterFloatConstruct();
             Renderer floatRenderer = RendererConstruct(floatCaster);
@@ -90,6 +111,7 @@ int main(int argc, char *args[])
             Uint64 tickCounter = SDL_GetPerformanceCounter();
             SDL_Event event;
 
+            // 創建 SDL 渲染器和紋理
             SDL_Renderer *sdlRenderer = SDL_CreateRenderer(
                 sdlWindow, -1,
                 SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
@@ -100,16 +122,21 @@ int main(int argc, char *args[])
                 sdlRenderer, SDL_PIXELFORMAT_ABGR8888,
                 SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
 
+            // 主循環
             while (!isExiting) {
+                // 更新遊戲和光線追踪器，獲取渲染的彩色緩衝區
                 RendererTraceFrame(&floatRenderer, &game, floatBuffer);
                 RendererTraceFrame(&fixedRenderer, &game, fixedBuffer);
 
+                // 渲染彩色緩衝區到窗口上
                 draw_buffer(sdlRenderer, fixedTexture, fixedBuffer, 0);
                 draw_buffer(sdlRenderer, floatTexture, floatBuffer,
                             SCREEN_WIDTH + 1);
 
+                // 更新 SDL 窗口
                 SDL_RenderPresent(sdlRenderer);
 
+                // 處理事件並更新遊戲狀態
                 if (SDL_PollEvent(&event)) {
                     isExiting =
                         process_event(&event, &moveDirection, &rotateDirection);
@@ -120,6 +147,7 @@ int main(int argc, char *args[])
                 GameMove(&game, moveDirection, rotateDirection,
                          ticks / (SDL_GetPerformanceFrequency() >> 8));
             }
+            // 釋放資源
             SDL_DestroyTexture(floatTexture);
             SDL_DestroyTexture(fixedTexture);
             SDL_DestroyRenderer(sdlRenderer);
--- raycaster_float.c
+++ raycaster_float.c
@@ -1,7 +1,6 @@
-// floating-point implementation for testing/comparison
+// 浮點數實現，用於測試/比較
 //
-// This diagram shows the coordinate system used in the
-// RayCasterFloat::Distance() function.
+// 這個圖表顯示了在 RayCasterFloat::Distance() 函數中使用的座標系統。
 //
 //              ^ rayA/
 //     sin-     |    /   sin+
@@ -24,12 +23,23 @@
     sqrt((float) (((x1) - (x2)) * ((x1) - (x2)) + \
                   ((y1) - (y2)) * ((y1) - (y2))))
 
+// 定義結構，表示浮點數光線追踪器的狀態
 typedef struct {
     float playerX;
     float playerY;
     float playerA;
 } RayCasterFloat;
 
+
+// 內部函數：RayCasterFloatTrace
+// 參數：rayCaster - 光線追踪器
+//       screenX - 螢幕上的 x 座標
+//       screenY - 螢幕上的 y 座標（輸出）
+//       textureNo - 貼圖編號（輸出）
+//       textureX - 貼圖中的 x 座標（輸出）
+//       textureY - 貼圖中的 y 座標（輸出）
+//       textureStep - 貼圖步進（輸出）
+// 說明：跟蹤光線，計算並輸出螢幕上的相應數據
 static void RayCasterFloatTrace(RayCaster *rayCaster,
                                 uint16_t screenX,
                                 uint8_t *screenY,
@@ -37,23 +47,38 @@ static void RayCasterFloatTrace(RayCaster *rayCaster,
                                 uint8_t *textureX,
                                 uint16_t *textureY,
                                 uint16_t *textureStep);
+// 內部函數：RayCasterFloatStart
+// 參數：rayCaster - 光線追踪器
+//       playerX - 玩家在 x 軸上的位置
+//       playerY - 玩家在 y 軸上的位置
+//       playerA - 玩家的角度
+// 說明：初始化浮點數光線追踪器的起始狀態
 static void RayCasterFloatStart(RayCaster *rayCaster,
                                 uint16_t playerX,
                                 uint16_t playerY,
                                 int16_t playerA);
+// 內部函數：RayCasterFloatDestruct
+// 參數：rayCaster - 光線追踪器
+// 說明：釋放浮點數光線追踪器的資源
 static void RayCasterFloatDestruct(RayCaster *rayCaster);
 
+// 函數：RayCasterFloatConstruct
+// 返回：RayCaster* - 光線追踪器結構指針
+// 說明：創建浮點光線追踪器實例
 RayCaster *RayCasterFloatConstruct(void)
 {
+    // 創建基本光線追踪器實例
     RayCaster *rayCaster = RayCasterConstruct();
     RayCasterFloat *rayCasterFloat = malloc(sizeof(RayCasterFloat));
 
+    // 檢查內存分配是否成功
     if (!rayCasterFloat) {
         rayCaster->Destruct(rayCaster);
         return NULL;
     }
     rayCaster->derived = rayCasterFloat;
 
+    // 設置函數指針
     rayCaster->Start = RayCasterFloatStart;
     rayCaster->Trace = RayCasterFloatTrace;
     rayCaster->Destruct = RayCasterFloatDestruct;
@@ -61,8 +86,14 @@ RayCaster *RayCasterFloatConstruct(void)
     return rayCaster;
 }
 
+// 內部函數：RayCasterFloatIsWall
+// 參數：rayX - 光線的 x 座標
+//       rayY - 光線的 y 座標
+// 返回：如果光線碰到牆壁，返回 true；否則返回 false
+// 說明：檢查光線是否碰到牆壁
 static bool RayCasterFloatIsWall(float rayX, float rayY)
 {
+    // 將浮點坐標轉換為地圖格子坐標
     float mapX = 0;
     float mapY = 0;
     modff(rayX, &mapX);
@@ -70,19 +101,31 @@ static bool RayCasterFloatIsWall(float rayX, float rayY)
     int tileX = (int) mapX;
     int tileY = (int) mapY;
 
+    // 檢查是否超出地圖邊界
     if (tileX < 0 || tileY < 0 || tileX >= MAP_X - 1 || tileY >= MAP_Y - 1) {
         return true;
     }
+
+    // 檢查格子中的位是否為牆壁
     return g_map[(tileX >> 3) + (tileY << (MAP_XS - 3))] &
            (1 << (8 - (tileX & 0x7)));
 }
 
+// 函數：RayCasterFloatDistance
+// 參數：playerX - 玩家的X座標
+//       playerY - 玩家的Y座標
+//       rayA - 光線的角度
+//       hitOffset - 命中點的偏移量
+//       hitDirection - 命中的方向（垂直或水平）
+// 返回：命中點到玩家的距離
+// 說明：計算光線與牆壁的交點，並返回命中點到玩家的距離
 static float RayCasterFloatDistance(float playerX,
                                     float playerY,
                                     float rayA,
                                     float *hitOffset,
                                     int *hitDirection)
 {
+    // 角度正規化，確保在0到2π之間
     while (rayA < 0) {
         rayA += 2.0f * M_PI;
     }
@@ -90,28 +133,33 @@ static float RayCasterFloatDistance(float playerX,
         rayA -= 2.0f * M_PI;
     }
 
+    // 計算光線的斜率和垂直斜率
+    // 計算tan和cot值
     float tanA = tan(rayA);
     float cotA = 1 / tanA;
     float rayX, rayY, vx, vy;
     float xOffset, yOffset, vertHitDis, horiHitDis;
     int depth = 0;
-    const int maxDepth = 100;  // maxDepth is the maximum number of
-                               // attempt for a ray to find a wall.
+    // 射線尋找牆壁的最大嘗試次數
+    const int maxDepth = 100;
 
-    // Check for vertical hit
+    // 檢查垂直方向的射線
     depth = 0;
     vertHitDis = 0;
-    if (sin(rayA) > 0.001) {  // rayA pointing rightward
+    if (sin(rayA) > 0.001) {
+        // 射線向右
         rayX = (int) playerX + 1;
         rayY = (rayX - playerX) * cotA + playerY;
         xOffset = 1;
         yOffset = xOffset * cotA;
-    } else if (sin(rayA) < -0.001) {  // rayA pointing leftward
+    } else if (sin(rayA) < -0.001) {
+        // 射線向左
         rayX = (int) playerX - 0.001;
         rayY = (rayX - playerX) * cotA + playerY;
         xOffset = -1;
         yOffset = xOffset * cotA;
-    } else {  // rayA pointing up or down
+    } else {
+        // 射線垂直
         rayX = playerX;
         rayY = playerY;
         xOffset = 0;
@@ -119,6 +167,7 @@ static float RayCasterFloatDistance(float playerX,
         depth = maxDepth;
     }
 
+    // 尋找垂直方向的牆壁
     while (depth < maxDepth) {
         if (RayCasterFloatIsWall(rayX, rayY)) {
             vertHitDis = P2P_DISTANCE(playerX, playerY, rayX, rayY);
@@ -132,20 +181,23 @@ static float RayCasterFloatDistance(float playerX,
     vx = rayX;
     vy = rayY;
 
-    // Check for horizontal hit
+    // 檢查水平方向的命中
     depth = 0;
     horiHitDis = 0;
-    if (cos(rayA) > 0.001) {  // rayA pointing upward
+    if (cos(rayA) > 0.001) {
+        // 射線向上
         rayY = (int) playerY + 1;
         rayX = (rayY - playerY) * tanA + playerX;
         yOffset = 1;
         xOffset = yOffset * tanA;
-    } else if (cos(rayA) < -0.001) {  // rayA pointing downward
+    } else if (cos(rayA) < -0.001) {
+        // 射線向下
         rayY = (int) playerY - 0.001;
         rayX = (rayY - playerY) * tanA + playerX;
         yOffset = -1;
         xOffset = yOffset * tanA;
-    } else {  // rayA pointing leftward or rightward
+    } else {
+        // 射線水平
         rayX = playerX;
         rayY = playerY;
         xOffset = 0;
@@ -153,6 +205,7 @@ static float RayCasterFloatDistance(float playerX,
         depth = maxDepth;
     }
 
+    // 尋找水平方向的牆壁
     while (depth < maxDepth) {
         if (RayCasterFloatIsWall(rayX, rayY)) {
             horiHitDis = P2P_DISTANCE(playerX, playerY, rayX, rayY);
@@ -164,19 +217,32 @@ static float RayCasterFloatDistance(float playerX,
         }
     }
 
-    if (vertHitDis < horiHitDis) {  // Vertical hit
+    // 選擇距離較短的命中點
+    if (vertHitDis < horiHitDis) {
+        // 垂直方向的命中
         rayX = vx;
         rayY = vy;
         *hitDirection = true;
         *hitOffset = rayY;
-    } else {  // Horizontal hit
+    } else {
+        // 水平方向的命中
         *hitDirection = false;
         *hitOffset = rayX;
     }
 
+    // 返回光線撞擊點到玩家位置的最小距離
     return fmin(vertHitDis, horiHitDis);
 }
 
+// 函數：RayCasterFloatTrace
+// 參數：rayCaster - 光線追踪器
+//       screenX - 螢幕上的 x 坐標
+//       screenY - 螢幕上的 y 坐標
+//       textureNo - 紋理編號
+//       textureX - 紋理 x 坐標
+//       textureY - 紋理 y 坐標
+//       textureStep - 紋理步進值
+// 說明：對浮點數光線追踪進行一次追踪，計算光線與牆面的交點及相關資訊
 static void RayCasterFloatTrace(RayCaster *rayCaster,
                                 uint16_t screenX,
                                 uint8_t *screenY,
@@ -187,25 +253,42 @@ static void RayCasterFloatTrace(RayCaster *rayCaster,
 {
     float hitOffset;
     int hitDirection;
+
+    // 計算光線的角度差
+    // 計算視點到屏幕上每個像素的距離和方向
     float deltaAngle = atanf(((int16_t) screenX - SCREEN_WIDTH / 2.0f) /
                              (SCREEN_WIDTH / 2.0f) * M_PI / 4);
+
+    // 使用浮點數距離函數計算光線的距離、偏移和方向
     float lineDistance = RayCasterFloatDistance(
         ((RayCasterFloat *) (rayCaster->derived))->playerX,
         ((RayCasterFloat *) (rayCaster->derived))->playerY,
         ((RayCasterFloat *) (rayCaster->derived))->playerA + deltaAngle,
         &hitOffset, &hitDirection);
+
+    // 計算實際牆面距離
+    // 計算真實距離（distance）和材質映射坐標（textureX）
     float distance = lineDistance * cos(deltaAngle);
     float dum;
     *textureX = (uint8_t) (256.0f * modff(hitOffset, &dum));
     *textureNo = hitDirection;
     *textureY = 0;
     *textureStep = 0;
+
+    // 如果距離大於0，則進行進一步計算
     if (distance > 0) {
+        // 計算螢幕上的投影高度
+        // 計算屏幕上像素的垂直位置（screenY）
         float tmp = INV_FACTOR / distance;
         *screenY = tmp;
+
+        // 計算材質的垂直映射和紋理步進（textureStep）
         float txs = (tmp * 2.0f);
+        // 計算紋理步進值
         if (txs != 0) {
             *textureStep = (256 / txs) * 256;
+
+            // 如果紋理投影超出螢幕高度，進行調整
             if (txs > SCREEN_HEIGHT) {
                 float wallHeight = (txs - SCREEN_HEIGHT) / 2;
                 *textureY = wallHeight * (256 / txs) * 256;
@@ -213,15 +296,23 @@ static void RayCasterFloatTrace(RayCaster *rayCaster,
             }
         }
     } else {
+        // 如果距離小於等於0，將螢幕上的投影高度設為0
         *screenY = 0;
     }
 }
 
+// 函數：RayCasterFloatStart
+// 參數：rayCaster - 光線追踪器
+//       playerX - 玩家的X座標
+//       playerY - 玩家的Y座標
+//       playerA - 玩家的角度
+// 說明：初始化光線追踪器的起始位置和角度
 static void RayCasterFloatStart(RayCaster *rayCaster,
                                 uint16_t playerX,
                                 uint16_t playerY,
                                 int16_t playerA)
 {
+    // 將初始玩家位置和角度轉換為浮點數表示
     ((RayCasterFloat *) (rayCaster->derived))->playerX =
         (playerX / 1024.0f) * 4.0f;
     ((RayCasterFloat *) (rayCaster->derived))->playerY =
@@ -230,6 +321,9 @@ static void RayCasterFloatStart(RayCaster *rayCaster,
         (playerA / 1024.0f) * 2.0f * M_PI;
 }
 
+// 函數：RayCasterFloatDestruct
+// 參數：rayCaster - 光線追踪器
+// 說明：釋放光線追踪器的資源
 static void RayCasterFloatDestruct(RayCaster *rayCaster)
 {
     free(rayCaster->derived);
